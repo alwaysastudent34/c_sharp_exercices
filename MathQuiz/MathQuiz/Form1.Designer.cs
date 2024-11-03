905a06d5cf8e347a5d42e4aa4177da68e1d259a6
alwaysastudent34 <musaarslannn34@gmail.com>
2024-11-03 13:31:24 +0100

    projects completed

---
+++ MathQuiz/MathQuiz/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace MathQuiz
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.NumericUpDown difference;
+        private System.Windows.Forms.Label timesLeftLabel;
+        private System.Windows.Forms.Label timesRightLabel;
+        private System.Windows.Forms.NumericUpDown product;
+        private System.Windows.Forms.Label dividedLeftLabel;
+        private System.Windows.Forms.Label dividedRightLabel;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.difference = new System.Windows.Forms.NumericUpDown();
+            this.timesLeftLabel = new System.Windows.Forms.Label();
+            this.timesRightLabel = new System.Windows.Forms.Label();
+            this.product = new System.Windows.Forms.NumericUpDown();
+            this.dividedLeftLabel = new System.Windows.Forms.Label();
+            this.dividedRightLabel = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
+            this.SuspendLayout();
+
+            // timeLabel
+            this.timeLabel.Location = new System.Drawing.Point(300, 20);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 23);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Time Left";
+
+            // plusLeftLabel
+            this.plusLeftLabel.Location = new System.Drawing.Point(50, 60);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(50, 23);
+            this.plusLeftLabel.TabIndex = 1;
+            this.plusLeftLabel.Text = "?";
+
+            // plusRightLabel
+            this.plusRightLabel.Location = new System.Drawing.Point(150, 60);
+            this.plusRightLabel.Name = "plusRightLabel";
+            this.plusRightLabel.Size = new System.Drawing.Size(50, 23);
+            this.plusRightLabel.TabIndex = 2;
+            this.plusRightLabel.Text = "?";
+
+            // label4 (for addition sign)
+            this.label4.Location = new System.Drawing.Point(100, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "+";
+
+            // label5 (for equal sign)
+            this.label5.Location = new System.Drawing.Point(200, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "=";
+
+            // sum
+            this.sum.Location = new System.Drawing.Point(250, 60);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(100, 22);
+            this.sum.TabIndex = 5;
+
+            // minusLeftLabel
+            this.minusLeftLabel.Location = new System.Drawing.Point(50, 100);
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            this.minusLeftLabel.Size = new System.Drawing.Size(50, 23);
+            this.minusLeftLabel.TabIndex = 6;
+            this.minusLeftLabel.Text = "?";
+
+            // minusRightLabel
+            this.minusRightLabel.Location = new System.Drawing.Point(150, 100);
+            this.minusRightLabel.Name = "minusRightLabel";
+            this.minusRightLabel.Size = new System.Drawing.Size(50, 23);
+            this.minusRightLabel.TabIndex = 7;
+            this.minusRightLabel.Text = "?";
+
+            // difference
+            this.difference.Location = new System.Drawing.Point(250, 100);
+            this.difference.Name = "difference";
+            this.difference.Size = new System.Drawing.Size(100, 22);
+            this.difference.TabIndex = 8;
+
+            // timesLeftLabel
+            this.timesLeftLabel.Location = new System.Drawing.Point(50, 140);
+            this.timesLeftLabel.Name = "timesLeftLabel";
+            this.timesLeftLabel.Size = new System.Drawing.Size(50, 23);
+            this.timesLeftLabel.TabIndex = 9;
+            this.timesLeftLabel.Text = "?";
+
+            // timesRightLabel
+            this.timesRightLabel.Location = new System.Drawing.Point(150, 140);
+            this.timesRightLabel.Name = "timesRightLabel";
+            this.timesRightLabel.Size = new System.Drawing.Size(50, 23);
+            this.timesRightLabel.TabIndex = 10;
+            this.timesRightLabel.Text = "?";
+
+            // product
+            this.product.Location = new System.Drawing.Point(250, 140);
+            this.product.Name = "product";
+            this.product.Size = new System.Drawing.Size(100, 22);
+            this.product.TabIndex = 11;
+
+            // dividedLeftLabel
+            this.dividedLeftLabel.Location = new System.Drawing.Point(50, 180);
+            this.dividedLeftLabel.Name = "dividedLeftLabel";
+            this.dividedLeftLabel.Size = new System.Drawing.Size(50, 23);
+            this.dividedLeftLabel.TabIndex = 12;
+            this.dividedLeftLabel.Text = "?";
+
+            // dividedRightLabel
+            this.dividedRightLabel.Location = new System.Drawing.Point(150, 180);
+            this.dividedRightLabel.Name = "dividedRightLabel";
+            this.dividedRightLabel.Size = new System.Drawing.Size(50, 23);
+            this.dividedRightLabel.TabIndex = 13;
+            this.dividedRightLabel.Text = "?";
+
+            // quotient
+            this.quotient.Location = new System.Drawing.Point(250, 180);
+            this.quotient.Name = "quotient";
+            this.quotient.Size = new System.Drawing.Size(100, 22);
+            this.quotient.TabIndex = 14;
+
+            // startButton
+            this.startButton.Location = new System.Drawing.Point(300, 300);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 23);
+            this.startButton.TabIndex = 15;
+            this.startButton.Text = "Start the quiz";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+
+            // timer1
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.timesLeftLabel);
+            this.Controls.Add(this.timesRightLabel);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.dividedLeftLabel);
+            this.Controls.Add(this.dividedRightLabel);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Math Quiz";
+
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ PictureViewer/PictureViewer/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+        private void InitializeCustomComponents()
+        {
+            // PictureBox oluşturma ve form üzerine ekleme
+            PictureBox pictureBox1 = new PictureBox
+            {
+                Name = "pictureBox1",
+                Size = new Size(200, 200),
+                Location = new Point(20, 20),
+                BorderStyle = BorderStyle.Fixed3D
+            };
+            this.Controls.Add(pictureBox1); // Form üzerine ekledik
+
+            // Show Button (Resim gösterme)
+            Button showButton = new Button
+            {
+                Name = "showButton",
+                Text = "Show a picture",
+                Location = new Point(20, 250)
+            };
+            showButton.Click += showButton_Click; // Olay işleyicisi ekleme
+            this.Controls.Add(showButton);
+
+            // Clear Button (Resmi temizleme)
+            Button clearButton = new Button
+            {
+                Name = "clearButton",
+                Text = "Clear the picture",
+                Location = new Point(120, 250)
+            };
+            clearButton.Click += clearButton_Click;
+            this.Controls.Add(clearButton);
+
+            // Background Color Button (Arka plan rengi değiştirme)
+            Button backgroundButton = new Button
+            {
+                Name = "backgroundButton",
+                Text = "Set background color",
+                Location = new Point(220, 250)
+            };
+            backgroundButton.Click += backgroundButton_Click;
+            this.Controls.Add(backgroundButton);
+
+            // Close Button (Formu kapatma)
+            Button closeButton = new Button
+            {
+                Name = "closeButton",
+                Text = "Close",
+                Location = new Point(350, 250)
+            };
+            closeButton.Click += closeButton_Click;
+            this.Controls.Add(closeButton);
+
+            // CheckBox (Resim modunu değiştirme - Stretch)
+            CheckBox checkBox1 = new CheckBox
+            {
+                Name = "checkBox1",
+                Text = "Stretch",
+                Location = new Point(20, 280)
+            };
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            this.Controls.Add(checkBox1);
+
+            // OpenFileDialog bileşeni
+            openFileDialog1 = new OpenFileDialog
+            {
+                Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*",
+                Title = "Select a picture file"
+            };
+
+            // ColorDialog bileşeni
+            colorDialog1 = new ColorDialog();
+        }
+
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user clicks OK, load the picture the user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the picture.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Show the color dialog box. If the user clicks OK, change the PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the user selects the Stretch check box, change the PictureBox's SizeMode property to "Stretch". If the user clears the check box, change it to "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+
+    }
+}
